fb495103666922166baaaf773d00c8c2f4f134c7
박승찬 <seungchan141414@gmail.com>
2023-12-11 16:59:17 +0900

    [Refactor] Ioc 제어를 위한 @Configuration, @Bean 애너테이션 추가
    @Configuration
    스프링이 빈 팩토리를 위한 오브젝트 설정을 담당하는 클래스라고 인식
    
    @Bean
    오브젝트를 만들어 주는 메서드
    
    userDao() 메서드는 UserDao 타입의 오브젝트를 생성하고 초기화해준다 => @Bean
    
    ConnectionMaker 타입의 오브젝트를 생성해주는 connectionMaker => @Bean
    
    #TestController
    AnnotationConfigApplicationContext을 이용해서 @Configuration으로 등록한
    클래스를 사용한다.
    
    getBean으로 사용하는 메서드는 DaoFactory.class에 있는 userDaoDConnection을 명시해 이용한다

--- StudySpringBoot3_1/src/main/java/springbook/user/TestController.java
+++ StudySpringBoot3_1/src/main/java/springbook/user/TestController.java
@@ -1,5 +1,7 @@
 package springbook.user;
 
+import org.apache.catalina.core.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import springbook.user.dao.*;
@@ -83,26 +85,40 @@ public class TestController {
 //        return list.toString();
 //    }
 
-    @GetMapping("user/dao/p89/test")
-    public String p72Test() throws SQLException, ClassNotFoundException {
-        // client(컨트롤러)에게 DConnectionMaker을 만드는 책임을 넘긴다.
-        NConnectionMaker nConnectionMaker = new NConnectionMaker();
-        DConnectionMaker dConnectionMaker = new DConnectionMaker();
+//    @GetMapping("user/dao/p89/test")
+//    public String p72Test() throws SQLException, ClassNotFoundException {
+//        // client(컨트롤러)에게 DConnectionMaker을 만드는 책임을 넘긴다.
+//        UserDao dUserDao = new DaoFactory().userDaoDConnection();
+//
+//        User user_D = new User("DUser", "박승찬", "married");
+//
+//        dUserDao.add(user_D);
+//
+//        User searchDUser = dUserDao.get(user_D.getId());
+//
+//        List<User> list = new ArrayList<>();
+//        list.add(searchDUser);
+//
+//        return list.toString();
+//    }
 
-        UserDao nUserDao = new DaoFactory().userDaoNConnection();
-        UserDao dUserDao = new DaoFactory().userDaoDConnection();
+    @GetMapping("user/dao/p97/test")
+    public String p97Test() throws SQLException, ClassNotFoundException {
+        // p97 Ioc 제어의 역전의 파트에서 DaoFactory#userDaoDConnection의 제어를 context에 의해 운영된다.
+        // DaoFactory.class에 있는 userDaoDConnection의 메서드를 Bean으로 등록
+        // Context을 이용해 객체생성 및 관리
+        // @Configuration으로 관리하는 클래스는 AnnotationConfigApplicationContext을 이용하여 관리한다.₩
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(DaoFactory.class);
+        UserDao dUserDao = context.getBean("userDaoDConnection", UserDao.class);
 
-        User user_N = new User("NUser", "박승찬", "married");
         User user_D = new User("DUser", "박승찬", "married");
 
-        nUserDao.add(user_N);
         dUserDao.add(user_D);
 
-        User searchNUser = nUserDao.get(user_N.getId());
         User searchDUser = dUserDao.get(user_D.getId());
 
         List<User> list = new ArrayList<>();
-        list.add(searchNUser);
         list.add(searchDUser);
 
         return list.toString();
--- StudySpringBoot3_1/src/main/java/springbook/user/dao/DaoFactory.java
+++ StudySpringBoot3_1/src/main/java/springbook/user/dao/DaoFactory.java
@@ -1,8 +1,12 @@
 package springbook.user.dao;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
 /**
  * 팩토리 클래스 UserDao을 생성 책임을 맡았다.
  */
+@Configuration // p 96 빈 팩토리를 위한 오브젝트 설정을 담당하는 클래스라고 인식.
 public class DaoFactory {
     /**
      * userDao 메소드를 호출하면 DConnectinMaker을 사용해 DB 커넥션을 가져오도록
@@ -13,17 +17,20 @@ public class DaoFactory {
      *
      * @return
      */
+    @Bean // p96 오브젝트를 만들어 주는 메서드
     public UserDao userDaoDConnection() {
         UserDao userDao = new UserDao(getConnectionMaker());
         return userDao;
     }
 
+    @Bean // p96 오브젝트를 만들어 주는 메서드
     public UserDao userDaoNConnection() {
         UserDao userDao = new UserDao(getConnectionMaker());
         return userDao;
     }
 
-    private NConnectionMaker getConnectionMaker() {
-        return new NConnectionMaker();
+    @Bean // p96 오브젝트를 만들어 주는 메서드
+    public ConnectionMaker getConnectionMaker() {
+        return new DConnectionMaker();
     }
 }
