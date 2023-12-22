cf197002b23549107f59116c0757f809c1fc04e1
박승찬 <seungchan141414@gmail.com>
2023-12-23 00:10:17 +0900

    [Feat] @Autowired 사용을 하여 필드 단계에서 DI 사용
    https://dmdrk1414.tistory.com/80
    
    @Autowired는
    인스턴스 변수가 있으면 테스트 컨텍스트 프레임워크는 변수 타입과 같은 컨텍스트 내의 빈을 찾는다.
    타입이 일치하는 빈이 있으면 인스턴스 변수에 주입
    
    @Autowired 사용방법
    @ExtendWith(SpringExtension.class)
    @ContextConfiguration(locations = "/xml/applicationContext.xml")
    public class UserDaoTest {
        // 타입 방식의 자동 와이어링
        // 만약 UserDao가 2개이면
        //      타입으로 가져올 빈 하나를 선택할 수 없는 경우에는 변수의 이름과 같은 이름의 빈이 있는지 확인
        //
        @Autowired
        private UserDao dao;
    이런식으로 @ContextConfiguration(locations = "/xml/applicationContext.xml") 을 등록한후
    
    <?xml version="1.0" encoding="UTF-8"?>
    <beans xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
           xmlns="http://www.springframework.org/schema/beans"
           xsi:schemaLocation="http://www.springframework.org/schema/beans http://www.springframework.org/schema/beans/spring-beans.xsd">
        <!--p131
        CountingDaoFactory#countConnectionMaker
        id: @Bean 이 있는 메서드 이름
        class: 리턴을 하는 클래스 패키지 명
        property: 프로퍼티 이름
        ref: 주입할 오브젝트를 정의한 빈의 ID.
        -->
        <bean id="dataSource" class="org.springframework.jdbc.datasource.SimpleDriverDataSource">
            <property name="driverClass" value="com.mysql.jdbc.Driver"/>
            <property name="url" value="jdbc:mysql://localhost/springbook?characterEncoding=UTF-8"/>
            <property name="username" value="root"/>
            <property name="password" value="qkrtmdcks1!"/>
        </bean>
        <bean id="userDao" class="springbook.user.dao.UserDao">
            <property name="dataSource" ref="dataSource"/>
        </bean>
    
    </beans>
    둘다 가능 하지만 같은 빈이 주입된다.
    
     @Autowired
     private UserDao dao;
     @Autowired
     private UserDao userDao;
    springbook.user.dao.UserDao@189b5fb1
    springbook.user.dao.UserDao@189b5fb1
    가능 둘다 같은 빈이 주입
    
     @Autowired
     private UserDao userDao;
     @Autowired
     private UserDao userDao2;
    springbook.user.dao.UserDao@189b5fb1
    springbook.user.dao.UserDao@189b5fb1
    가능 둘다 같은 빈이 주입
    
     @Autowired
     private UserDao use1;
     @Autowired
     private UserDao use2;
    springbook.user.dao.UserDao@189b5fb1
    springbook.user.dao.UserDao@189b5fb1
    만약 xml에 같은 타입에 2개의 빈이 있다면
        <bean id="userDao" class="springbook.user.dao.UserDao">
            <property name="dataSource" ref="dataSource"/>
        </bean>
    
        <bean id="userDao2" class="springbook.user.dao.UserDao">
            <property name="dataSource" ref="dataSource"/>
        </bean>
        @Autowired
        private UserDao userDao;
        @Autowired
        private UserDao userDao2;
    이렇게 변수 명을 명시 해줘라

--- StudySpringBoot3_1/src/test/java/UserDaoTest.java
+++ StudySpringBoot3_1/src/test/java/UserDaoTest.java
@@ -1,17 +1,15 @@
-import org.aspectj.lang.annotation.Before;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.GenericXmlApplicationContext;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import springbook.user.dao.UserDao;
 import springbook.user.domain.User;
 
+import javax.sql.DataSource;
 import java.sql.SQLException;
 
 import static org.assertj.core.api.Assertions.*;
@@ -20,7 +18,18 @@ import static org.hamcrest.CoreMatchers.is;
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(locations = "/xml/applicationContext.xml")
 public class UserDaoTest {
-    private UserDao dao;
+    // 타입 방식의 자동 와이어링
+    // 만약 UserDao가 2개이면
+    //      타입으로 가져올 빈 하나를 선택할 수 없는 경우에는 변수의 이름과 같은 이름의 빈이 있는지 확인
+    //
+    @Autowired
+    private UserDao userDao;
+    @Autowired
+    private UserDao userDao2;
+    @Autowired
+    private DataSource source;
+    @Autowired
+    private DataSource dataSource;
     private User user1;
     private User user2;
     private User user3;
@@ -29,28 +38,30 @@ public class UserDaoTest {
 
     @BeforeEach
     public void setUp() {
-        dao = this.context.getBean("userDao", UserDao.class);
-
         this.user1 = new User("gyumee", "박성철", "springno1");
         this.user2 = new User("leegw700", "이길원", "springno2");
         this.user3 = new User("bumhin", "박범진", "springno3");
+
+        System.out.println(userDao);
+        System.out.println(userDao2);
+        System.out.println();
     }
 
     @Test
     public void addAndGet() throws Exception {
         // given
         // 테스트를 위한 deleteAll 추가
-        dao.deleteAll();
-        assertThat(dao.getCount()).isEqualTo(0);
+        userDao.deleteAll();
+        assertThat(userDao.getCount()).isEqualTo(0);
 
         User user = new User("dmdrk1414", "박승찬", "1234");
 
         // when
-        dao.add(user);
+        userDao.add(user);
 
-        assertThat(dao.getCount()).isEqualTo(1);
+        assertThat(userDao.getCount()).isEqualTo(1);
 
-        User user2 = dao.get(user.getId());
+        User user2 = userDao.get(user.getId());
 
         // then
         assertThat(user2.getName()).contains(user.getName());
@@ -63,29 +74,29 @@ public class UserDaoTest {
 
         // when
         // then
-        dao.deleteAll();
-        assertThat(dao.getCount()).isEqualTo(0);
+        userDao.deleteAll();
+        assertThat(userDao.getCount()).isEqualTo(0);
 
-        dao.add(user1);
-        assertThat(dao.getCount()).isEqualTo(1);
+        userDao.add(user1);
+        assertThat(userDao.getCount()).isEqualTo(1);
 
-        dao.add(user2);
-        assertThat(dao.getCount()).isEqualTo(2);
+        userDao.add(user2);
+        assertThat(userDao.getCount()).isEqualTo(2);
 
-        dao.add(user3);
-        assertThat(dao.getCount()).isEqualTo(3);
+        userDao.add(user3);
+        assertThat(userDao.getCount()).isEqualTo(3);
     }
 
     @Test
     public void getUserFailure() throws SQLException {
         // given
-        dao.deleteAll();
+        userDao.deleteAll();
 
         // then
-        assertThat(dao.getCount()).isEqualTo(0);
+        assertThat(userDao.getCount()).isEqualTo(0);
 
         String searchId = "unknown_id";
-        assertThatThrownBy(() -> dao.get(searchId))
+        assertThatThrownBy(() -> userDao.get(searchId))
                 .isInstanceOf(EmptyResultDataAccessException.class);
     }
 }
