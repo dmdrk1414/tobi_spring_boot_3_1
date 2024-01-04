965b9486d58d107ce90afbd82c80adc6a7580133
박승찬 <seungchan141414@gmail.com>
2024-01-04 23:41:52 +0900

    [Feat] 헤드퍼스트 p57 전략패턴 set을 이용한 할당 당법
    인터패이스를 이용한 느슨한 연결

--- StudySpringBoot3_1/src/main/java/springbook/user/headfirst/strategy/Duck.java
+++ StudySpringBoot3_1/src/main/java/springbook/user/headfirst/strategy/Duck.java
@@ -20,6 +20,14 @@ public abstract class Duck {
         quackBehavior.quack();
     }
 
+    public void setFlyBehavior(FlyBehavior flyBehavior) {
+        this.flyBehavior = flyBehavior;
+    }
+
+    public void setQuackBehavior(QuackBehavior quackBehavior) {
+        this.quackBehavior = quackBehavior;
+    }
+
     public void swim() {
         System.out.println("모든 오리는 물에 뜸니다. 가짜 오리도 뜨죠");
     }
--- StudySpringBoot3_1/src/test/java/springbook/user/headfirst/strategy/MallardDuckTest.java
+++ StudySpringBoot3_1/src/test/java/springbook/user/headfirst/strategy/MallardDuckTest.java
@@ -1,6 +1,7 @@
 package springbook.user.headfirst.strategy;
 
 import org.junit.jupiter.api.Test;
+import springbook.user.headfirst.strategy.fly.FlyRocketPowered;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -10,6 +11,17 @@ class MallardDuckTest {
     void display() {
         Duck mallard = new MallardDuck();
         mallard.performQuack();
+        mallard.performFly();
+    }
+
+    @Test
+    void display_set_test() {
+        Duck mallard = new MallardDuck();
+        mallard.performQuack();
+        mallard.performFly();
+
+        System.out.println();
+        mallard.setFlyBehavior(new FlyRocketPowered());
         mallard.performFly();
     }
 }
