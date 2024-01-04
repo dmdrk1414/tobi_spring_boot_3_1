d9c415de44d1441f4b87b803c2bd674152a7d46c
박승찬 <seungchan141414@gmail.com>
2024-01-04 17:03:19 +0900

    [Feat] p212. try/catch/finally 구분으로 예외 처리하는 방법
    - close을 하기전에 null 검사를 필수적으로 해야한다.
    왜냐하면 null인데 close을 하면 null 예외가 발생하기 때문
    
    - close에는 try/catch가 없어도 되지 않을까 생각할 수도 있지만
    반드시 있어야한다. try/catch 블록 없이 ps.close()를 처리하다가 예외가 발생하면 아래 c.close() 부분이 실행되지 않고 메소드를 빠져나가는 문제가 발생하기 때문

--- StudySpringBoot3_1/src/main/java/springbook/user/dao/UserDao.java
+++ StudySpringBoot3_1/src/main/java/springbook/user/dao/UserDao.java
@@ -71,26 +71,79 @@ public class UserDao {
     public void deleteAll() throws SQLException {
         Connection connection = dataSource.getConnection();
 
-        PreparedStatement preparedStatement = connection.prepareStatement("DELETE FROM users");
-        preparedStatement.execute();
+        PreparedStatement preparedStatement = null;
+        try {
+            // 예외가 발생할 가능성이 있는 코드를
+            // 모두 try 블록으로 묶어준다.
+            preparedStatement = connection.prepareStatement("DELETE FROM users");
+            preparedStatement.execute();
+        } catch (SQLException e) {
+            // 예외가 발생했을때 부가적인 작업을 해줄 수 있도록 catch 블록을 둔다.
+            // 아직은 예외를 다시 메소드 밖으로 던지는 것밖에 없다.
+            throw e;
+        } finally {
+            // finally이므로 try 블록에서 예외가 발생했을 때나 안 했을때나 모두 실행된다.
+            if (preparedStatement != null) {
+                try {
+                    preparedStatement.close();
+                } catch (SQLException e) {
+                    // ps.close() 메소드에서도 SQLException 이 발생할 수 있기 때문에
+                    // 이를 잡아줘야 한다.
+                    // 그렇지 않으면 Connection을 close() 하지 못하고 메소드를 빠져날갈 수 있다.
+                }
+            }
 
-        preparedStatement.close();
-        connection.close();
+            if (connection != null) {
+                try {
+                    // Connection 반환
+                    connection.close();
+                } catch (SQLException e) {
+                }
+            }
+        }
     }
 
     public int getCount() throws SQLException {
-        Connection connection = dataSource.getConnection();
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+        try {
+            connection = dataSource.getConnection();
 
-        PreparedStatement preparedStatement = connection.prepareStatement("SELECT count(*) FROM users");
+            preparedStatement = connection.prepareStatement("SELECT count(*) FROM users");
 
-        ResultSet resultSet = preparedStatement.executeQuery();
-        resultSet.next();
-        int count = resultSet.getInt(1);
+            resultSet = preparedStatement.executeQuery();
+            resultSet.next();
+            return resultSet.getInt(1);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            if (resultSet != null) {
+                try {
+                    resultSet.close();
+                } catch (SQLException e) {
+                    // ps.close() 메소드에서도 SQLException 이 발생할 수 있기 때문에
+                    // 이를 잡아줘야 한다.
+                    // 그렇지 않으면 Connection을 close() 하지 못하고 메소드를 빠져날갈 수 있다.
+                }
+            }
+            if (preparedStatement != null) {
+                try {
+                    preparedStatement.close();
+                } catch (SQLException e) {
+                    // ps.close() 메소드에서도 SQLException 이 발생할 수 있기 때문에
+                    // 이를 잡아줘야 한다.
+                    // 그렇지 않으면 Connection을 close() 하지 못하고 메소드를 빠져날갈 수 있다.
+                }
+            }
 
-        resultSet.close();
-        preparedStatement.close();
-        connection.close();
-
-        return count;
+            if (connection != null) {
+                try {
+                    // Connection 반환
+                    connection.close();
+                } catch (SQLException e) {
+                }
+            }
+        }
     }
 }
