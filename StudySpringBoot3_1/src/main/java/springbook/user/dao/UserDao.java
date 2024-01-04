07e89e8191c7ea434bc3e1c429b564455861c0d9
박승찬 <seungchan141414@gmail.com>
2024-01-04 20:44:24 +0900

    [Feat] p222 StatementStrategy을 만드는 부분에서 독립
    클라이언트가 컨텍스트를 호출할 때 넘겨줄 전략 파라미터
    클라이언트가 컨텍스트를 호출 StatemtmentStrategy을 선택할 수 있다

--- StudySpringBoot3_1/src/main/java/springbook/user/dao/UserDao.java
+++ StudySpringBoot3_1/src/main/java/springbook/user/dao/UserDao.java
@@ -69,39 +69,11 @@ public class UserDao {
     }
 
     public void deleteAll() throws SQLException {
-        Connection connection = dataSource.getConnection();
+        // 선정한 전략 클래스의 오브젝트 생성
+        StatementStrategy strategy = new DeleteAllStatement();
 
-        PreparedStatement preparedStatement = null;
-        try {
-            // 예외가 발생할 가능성이 있는 코드를
-            // 모두 try 블록으로 묶어준다.
-            StatementStrategy strategy = new DeleteAllStatement();
-            preparedStatement = strategy.makePreparedStatement(connection);
-            preparedStatement.execute();
-        } catch (SQLException e) {
-            // 예외가 발생했을때 부가적인 작업을 해줄 수 있도록 catch 블록을 둔다.
-            // 아직은 예외를 다시 메소드 밖으로 던지는 것밖에 없다.
-            throw e;
-        } finally {
-            // finally이므로 try 블록에서 예외가 발생했을 때나 안 했을때나 모두 실행된다.
-            if (preparedStatement != null) {
-                try {
-                    preparedStatement.close();
-                } catch (SQLException e) {
-                    // ps.close() 메소드에서도 SQLException 이 발생할 수 있기 때문에
-                    // 이를 잡아줘야 한다.
-                    // 그렇지 않으면 Connection을 close() 하지 못하고 메소드를 빠져날갈 수 있다.
-                }
-            }
-
-            if (connection != null) {
-                try {
-                    // Connection 반환
-                    connection.close();
-                } catch (SQLException e) {
-                }
-            }
-        }
+        // 컨텍스트 호출, 전략 오브젝트 전달
+        jdbcContextWithStatementStrategy(strategy);
     }
 
     public int getCount() throws SQLException {
@@ -128,6 +100,42 @@ public class UserDao {
                     // 그렇지 않으면 Connection을 close() 하지 못하고 메소드를 빠져날갈 수 있다.
                 }
             }
+            if (preparedStatement != null) {
+                try {
+                    preparedStatement.close();
+                } catch (SQLException e) {
+                    // ps.close() 메소드에서도 SQLException 이 발생할 수 있기 때문에
+                    // 이를 잡아줘야 한다.
+                    // 그렇지 않으면 Connection을 close() 하지 못하고 메소드를 빠져날갈 수 있다.
+                }
+            }
+
+            if (connection != null) {
+                try {
+                    // Connection 반환
+                    connection.close();
+                } catch (SQLException e) {
+                }
+            }
+        }
+    }
+
+    public void jdbcContextWithStatementStrategy(StatementStrategy strategy) throws SQLException {
+        Connection connection = null;
+
+        PreparedStatement preparedStatement = null;
+        try {
+            connection = dataSource.getConnection();
+            // 예외가 발생할 가능성이 있는 코드를
+            // 모두 try 블록으로 묶어준다.
+            preparedStatement = strategy.makePreparedStatement(connection);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            // 예외가 발생했을때 부가적인 작업을 해줄 수 있도록 catch 블록을 둔다.
+            // 아직은 예외를 다시 메소드 밖으로 던지는 것밖에 없다.
+            throw e;
+        } finally {
+            // finally이므로 try 블록에서 예외가 발생했을 때나 안 했을때나 모두 실행된다.
             if (preparedStatement != null) {
                 try {
                     preparedStatement.close();
