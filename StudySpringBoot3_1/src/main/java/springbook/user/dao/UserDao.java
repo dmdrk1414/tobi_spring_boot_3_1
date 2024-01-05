b973a2bd42a258e2e417bde8d0b0d9da7efbc524
박승찬 <seungchan141414@gmail.com>
2024-01-05 13:18:23 +0900

    [Feat] p.226 고정된건 고정하고, 변하는 것만 적용한다
    외부에서 user 객체를 받고 변하는 부분만 클래스를(AddStatement)을 생성해서 적용

--- StudySpringBoot3_1/src/main/java/springbook/user/dao/UserDao.java
+++ StudySpringBoot3_1/src/main/java/springbook/user/dao/UserDao.java
@@ -16,22 +16,8 @@ public class UserDao {
     }
 
     public void add(User user) throws SQLException, ClassNotFoundException {
-        Connection connection = dataSource.getConnection();
-
-        // SQL을 담은 Statement(또는 PreparedStatement)을 만든다.
-        PreparedStatement preparedStatement = connection.prepareStatement(
-                "INSERT INTO users(id, name, password) VALUES (?, ?, ?) "
-        );
-        preparedStatement.setString(1, user.getId());
-        preparedStatement.setString(2, user.getName());
-        preparedStatement.setString(3, user.getPassword());
-
-        // 만들어진 Statement을 실행한다.
-        preparedStatement.execute();
-
-        // 작업 중에 생성된 Connection, Statement, ResultSet 같은 리소리는 작업을 마친 후 반드시 닫아준다.
-        preparedStatement.close();
-        connection.close();
+        StatementStrategy strategy = new AddStatement(user);
+        jdbcContextWithStatementStrategy(strategy);
     }
 
     public User get(String id) throws ClassNotFoundException, SQLException {
