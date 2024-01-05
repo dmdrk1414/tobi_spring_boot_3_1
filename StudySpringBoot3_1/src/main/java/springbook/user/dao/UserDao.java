9965fc96086c05df0565affffa3582f54fc14579
박승찬 <seungchan141414@gmail.com>
2024-01-05 16:16:58 +0900

    [Feat] p.229 내부 클래스, 로컬 클래스, nested class을 이용한 AddStatement 만들기
    변하는 부분을 변하고, 고정된 부분을 고정시킨다.

--- StudySpringBoot3_1/src/main/java/springbook/user/dao/UserDao.java
+++ StudySpringBoot3_1/src/main/java/springbook/user/dao/UserDao.java
@@ -16,6 +16,26 @@ public class UserDao {
     }
 
     public void add(User user) throws SQLException, ClassNotFoundException {
+        class AddStatement implements StatementStrategy {
+            User user;
+
+            public AddStatement(User user) {
+                this.user = user;
+            }
+
+            @Override
+            public PreparedStatement makePreparedStatement(Connection connection) throws SQLException {
+                PreparedStatement preparedStatement = connection.prepareStatement(
+                        "INSERT INTO users(id, name, password) VALUES (?, ?, ?)"
+                );
+
+                preparedStatement.setString(1, user.getId());
+                preparedStatement.setString(2, user.getName());
+                preparedStatement.setString(3, user.getPassword());
+
+                return preparedStatement;
+            }
+        }
         StatementStrategy strategy = new AddStatement(user);
         jdbcContextWithStatementStrategy(strategy);
     }
