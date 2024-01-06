948b4e7fefaeb4ec5333c68dcf127d2a522e530b
박승찬 <seungchan141414@gmail.com>
2024-01-06 14:22:13 +0900

    [Feat] p.253 @BeforEach을 이용한 테스트 픽스처 분리 작업

--- StudySpringBoot3_1/src/test/java/springbook/user/learningtest/template/CalculatorTest.java
+++ StudySpringBoot3_1/src/test/java/springbook/user/learningtest/template/CalculatorTest.java
@@ -1,5 +1,6 @@
 package springbook.user.learningtest.template;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
@@ -10,13 +11,23 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
+    Calculator calculator;
+    String numFilepath;
+
+    @BeforeEach
+    public void setUp() {
+        try {
+            this.calculator = new Calculator();
+            Resource resource = new ClassPathResource("numbers.txt");
+            this.numFilepath = resource.getURI().getPath();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     @Test
     void sumOfNumbers() throws IOException {
-        Calculator calculator = new Calculator();
-        Resource resource = new ClassPathResource("numbers.txt");
-
-        int sum = calculator.calcSum(resource.getURI().getPath());
+        int sum = calculator.calcSum(numFilepath);
 
         assertThat(sum).isEqualTo(10);
     }
