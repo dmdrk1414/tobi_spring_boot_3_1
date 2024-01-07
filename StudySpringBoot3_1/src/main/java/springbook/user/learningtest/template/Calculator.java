8dda50b3fe66642affd59ef48a09521998633036
박승찬 <seungchan141414@gmail.com>
2024-01-07 17:25:17 +0900

    [Feat] p257 제네릭스를 이용한 콜백 인터페이스
    제네릭스(Generics)를 이용하여 다양한 오브젝트 타입을 지원하는 인터페이스나 메소드를 정의

--- StudySpringBoot3_1/src/main/java/springbook/user/learningtest/template/Calculator.java
+++ StudySpringBoot3_1/src/main/java/springbook/user/learningtest/template/Calculator.java
@@ -5,11 +5,11 @@ import java.io.FileReader;
 import java.io.IOException;
 
 public class Calculator {
-    public Integer lineReadTemplate(String filepath, LineCallback callback, int initVal) throws IOException {
+    public <T> T lineReadTemplate(String filepath, LineCallback<T> callback, T initVal) throws IOException {
         BufferedReader br = null;
         try {
             br = new BufferedReader(new FileReader(filepath));
-            Integer res = initVal;
+            T res = initVal;
             String line = null;
             while ((line = br.readLine()) != null) {
                 res = callback.doSomethingWithLine(line, res);
@@ -33,7 +33,7 @@ public class Calculator {
     public Integer calcSum(String filepath) throws IOException {
 
         LineCallback sumCallback =
-                new LineCallback() {
+                new LineCallback<Integer>() {
                     @Override
                     public Integer doSomethingWithLine(String line, Integer value) {
                         return value + Integer.valueOf(line);
@@ -44,7 +44,7 @@ public class Calculator {
 
     public Integer calcMultiply(String filepath) throws IOException {
         LineCallback sumCallback =
-                new LineCallback() {
+                new LineCallback<Integer>() {
                     @Override
                     public Integer doSomethingWithLine(String line, Integer value) {
                         return value * Integer.valueOf(line);
