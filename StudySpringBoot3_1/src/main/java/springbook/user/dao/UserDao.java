3c7a7240025247e81f48889c5eaa28259e2ed488
박승찬 <seungchan141414@gmail.com>
2024-01-09 20:33:24 +0900

    [Feat] p. 304 userDao 인터페이스 적용
    가장 중요한 이유는 데이터액세스 로직을 담은 코드를 성격이 다른 코드에서 분리해놓기 위해서이다.
    DAO를 사용하는 쪽에서는 DAO가 내부에서 어떤 데이터 액세스 기술을 사용하는지 신경 쓰지 않아도 된다.
    User와 같은 자바빈으로 만들어진, 특정 기술에 독립적인 단순한 오브젝트를 주고받으면서
    데이터 액세스 기능을 사용하기만 하면 된다.
    그런 면에서 DAO는 인터페이스를 사용해 구체적인 클래스 정보와 구현 방법을 감추고, DI를 통해 제공되도록 만드는 것이 바람직하

--- StudySpringBoot3_1/src/main/java/springbook/user/dao/UserDao.java
+++ StudySpringBoot3_1/src/main/java/springbook/user/dao/UserDao.java
@@ -1,116 +1,17 @@
 package springbook.user.dao;
 
-import com.mysql.cj.exceptions.MysqlErrorNumbers;
-import lombok.NoArgsConstructor;
-import org.springframework.dao.DataAccessException;
-import org.springframework.dao.DuplicateKeyException;
-import org.springframework.dao.EmptyResultDataAccessException;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.PreparedStatementCreator;
-import org.springframework.jdbc.core.ResultSetExtractor;
-import org.springframework.jdbc.core.RowMapper;
 import springbook.user.domain.User;
-import springbook.user.exception.DuplicateUserIdException;
 
-import javax.sql.DataSource;
-import java.sql.*;
 import java.util.List;
 
-// TODO: 1/7/24
-// p275
-// 1. userMapper을 DI용 프로퍼티로 만들고 싶다.
-// 2. sql을 외부에서 가져오고싶다.
-@NoArgsConstructor // 빈을 사용하기 위해 기본 생성자 생성
-public class UserDao {
-    private JdbcTemplate jdbcTemplate;
-    private RowMapper<User> userMapper =
-            new RowMapper<User>() {
-                @Override
-                public User mapRow(ResultSet rs, int rowNum) throws SQLException {
-                    User user = new User();
-                    user.setId(rs.getString("id"));
-                    user.setName(rs.getString("name"));
-                    user.setPassword(rs.getString("password"));
-                    return user;
-                }
-            };
+public interface UserDao {
+    void add(User user);
 
-    public void setDataSource(DataSource dataSource) {
-        this.jdbcTemplate = new JdbcTemplate(dataSource);
-    }
+    User get(String id);
 
-    /**
-     * 만약 jdbcTemplate을 DI으로 이용해 주입할 생각이면
-     *
-     * @param jdbcTemplate
-     */
-    public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
-        // 만약 DI으로 주입하고 싶다면
-        // JdbcTemplate을 -> JdbcOperations interface을 이용해 구현을 한다.
-        this.jdbcTemplate = jdbcTemplate;
-    }
+    List<User> getAll();
 
+    void deleteAll();
 
-    public void add(final User user) throws DuplicateUserIdException {
-        try {
-            String sql = "INSERT  INTO users(id, name, password) VALUES (?, ?, ?)";
-            this.jdbcTemplate.update(sql, user.getId(), user.getName(), user.getPassword());
-        } catch (DuplicateKeyException e) {
-            // DataAccessException의 상속을 받는 DuplicateKeyException을 try으로 처리한다.
-            throw new DuplicateUserIdException(e);
-        }
-    }
-
-    public User get(String id) throws ClassNotFoundException, SQLException {
-        String sql = "SELECT * FROM users WHERE id = ?";
-        return this.jdbcTemplate.queryForObject(sql,
-                // sql에 바인딩할 파라미터 값, 가변인자 대신 배열을 사용한다.
-                new Object[]{id},
-                this.userMapper
-        );
-    }
-
-    public void deleteAll() {
-        String sql = "DELETE FROM users";
-        this.jdbcTemplate.update(sql);
-
-        // 콜백 형식의 jdbcTemplate.update 사용방법
-//        this.jdbcTemplate.update(
-//                new PreparedStatementCreator() {
-//                    @Override
-//                    public PreparedStatement createPreparedStatement(Connection con) throws SQLException {
-//                        return con.prepareStatement("DELETE FROM users");
-//                    }
-//                }
-//        );
-    }
-
-
-    public int getCount() throws SQLException {
-        String sql = "SELECT  COUNT(*) FROM users";
-        return this.jdbcTemplate.queryForObject(sql, Integer.class);
-//        return this.jdbcTemplate.query(
-//                new PreparedStatementCreator() {
-//                    @Override
-//                    public PreparedStatement createPreparedStatement(Connection con) throws SQLException {
-//                        return con.prepareStatement("SELECT  COUNT(*) FROM users");
-//                    }
-//                },
-//                new ResultSetExtractor<Integer>() {
-//                    // ResultSet에서 추출하는 값은 다양하기 때문에 타입을 지정해준다.
-//                    @Override
-//                    public Integer extractData(ResultSet rs) throws SQLException, DataAccessException {
-//                        rs.next();
-//                        return rs.getInt(1);
-//                    }
-//                }
-//        );
-    }
-
-    public List<User> getAll() {
-        return this.jdbcTemplate.query("SELECT * FROM users order by id",
-                // query의 리턴 타입은 List<T>이다.
-                // query()는 제네릭 메소드로 타입은 파라미터로 넘기는 RowMapper<T> 콜백 오브젝트에서 결정된다.
-                this.userMapper);
-    }
+    int getCount();
 }
